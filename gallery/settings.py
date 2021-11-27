5118764396e8012af110eaa5d291e3df6228b058
khalid-serar <khalid.serar@student.moringaschool.com>
2021-11-27 11:05:48 +0300

    add bootstrap

--- gallery/settings.py
+++ gallery/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'photos.apps.PhotosConfig',
+    'bootstrap3'
     'cloudinary',
 ]
 
@@ -121,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
