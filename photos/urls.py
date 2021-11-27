4608cd2dd80b0366f9540820be580c44e33a1e21
khalid-serar <khalid.serar@student.moringaschool.com>
2021-11-27 11:00:25 +0300

    add changes

--- photos/urls.py
+++ photos/urls.py
@@ -1,7 +1,16 @@
+from django.conf.urls import url
 from django.urls import path
-from django.urls.resolvers import URLPattern
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
-urlpatterns= [
-    path('', views.welcome, name='welcome')
+
+urlpatterns=[
+   url('^$',views.gallery,name='gallery'),
+    path('photo/<str:pk>',views.viewPhoto,name='photo'),
+    url('add',views.addPhoto,name='add'),
+    url('about',views.about,name='about'),
+    url(r'^search/', views.search_results, name='search_results')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
