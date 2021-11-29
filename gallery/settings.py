4432df92f085f92f3297d4a95fe73e738cb6332c
khalid-serar <khalid.serar@student.moringaschool.com>
2021-11-29 10:08:54 +0300

    add changes in settings.file

--- gallery/settings.py
+++ gallery/settings.py
@@ -131,6 +131,10 @@ PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
